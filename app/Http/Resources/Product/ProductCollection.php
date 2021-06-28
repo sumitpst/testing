1c542f9d21d75fbce77395c0747ba78dc4379a96
Sumit <sumitpst@gmail.com>
2021-06-28 17:14:40 +0530

    product collection done

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -14,6 +14,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name' => $this->name,
+            'totalPrice' => round(( 1 - ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'discount' => $this->discount,
+            'href' => [
+                'link' => route('products.show',$this->id)
+            ]
+        ];
     }
 }
